387fd4e9645a837381189358bc67ec824d3c9ded
ilyast2700 <44866736+ilyast2700@users.noreply.github.com>
2019-09-22 18:12:12 +0300

    New fixes

---
+++ javascript/pop-up.js
@@ -0,0 +1,13 @@
+let re = new RegExp('w');
+inputOne.onblur = function() {
+  if (this.value === '') {
+    this.classList.add('form-first-warning-active');
+    errorOne.innerHTML = 'Please enter your name.';
+  }
+};
+
+inputOne.onfocus = function() {
+  if (this.classList.contains('form-first-warning-active')) {
+    this.classList.remove('.form-first-warning-active');
+    errorOne.innerHTML = "";
+  }
